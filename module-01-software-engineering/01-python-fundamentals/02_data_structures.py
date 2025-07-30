ee1241c37e4db704d56a7177c89da7bdcd9ebebb
janainacazuza <janainamartinscazuza@gmail.com>
2025-07-29 21:34:35 -0300

    feat: add tuple and dictionary exercises

---
+++ module-01-software-engineering/01-python-fundamentals/02_data_structures.py
@@ -0,0 +1,13 @@
+secret_cordinates = (-20.1452, -44.8857)
+weapon_sheet = {
+    "name": "Widowmaker",
+    "type": "sword",
+    "weight": 2.5,
+    "material": "valerian steel",
+    "available": True,
+}
+print(f"Longitude:{secret_cordinates[1]}")
+
+print(f"Weapon type: {weapon_sheet['type']}")
+weapon_sheet["rarity"] = "legendary"
+print(f"Weapon sheets:{weapon_sheet}")
